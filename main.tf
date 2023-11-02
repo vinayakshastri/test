87e98193a26cc88d853b317105759f058b71d776
Vinayak Shastri <44750730+vinayakshastri@users.noreply.github.com>
2023-11-02 15:17:17 +1030

    Update

--- main.tf
+++ main.tf
@@ -8,6 +8,31 @@ resource "aws_elastic_beanstalk_application" "this" {
   description = "A sample Elastic Beanstalk application"
 }
 
+resource "aws_iam_role" "eb_instance_role" {
+  name = "eb-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eb_instance_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+  role       = aws_iam_role.eb_instance_role.name
+}
+
+resource "aws_iam_instance_profile" "eb_instance_profile" {
+  name = "eb-instance-profile"
+  role = aws_iam_role.eb_instance_role.name
+}
+
 resource "aws_elastic_beanstalk_environment" "this" {
   name        = "sample-app-env"
   application = aws_elastic_beanstalk_application.this.name
@@ -17,5 +42,11 @@ resource "aws_elastic_beanstalk_environment" "this" {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
     value     = "SingleInstance"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.eb_instance_profile.name
   }
 }
