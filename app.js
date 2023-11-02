7fec432646137938411ae55aa1b9229985aedd98
Vinayak Shastri <44750730+vinayakshastri@users.noreply.github.com>
2023-11-02 13:41:25 +1030

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => res.send('Hello from Elastic Beanstalk!'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
